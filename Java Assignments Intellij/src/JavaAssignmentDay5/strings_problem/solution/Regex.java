1de588bc9495bb8759182aaaee5fc1c8f23f6dc7
shrutisinha <shrutisinha952@gmail.com>
2017-07-13 17:37:35 +0530

    Update Regex.java

--- Java Assignments Intellij/src/JavaAssignmentDay5/strings_problem/solution/Regex.java
+++ Java Assignments Intellij/src/JavaAssignmentDay5/strings_problem/solution/Regex.java
@@ -13,12 +13,6 @@ public class Regex{
 	*@return True if str starts with Capital Letter and ends in period.
 	*/
 	public boolean checkPattern(String str){
-		/* ^matches with beginning of line
-		*  [A-Z] matches uppercase character from A through Z
-		*  .* '.' mathes any character and '*' means 0 or more time, .*  matches any character sequence
-		*  [\\.] matches '.' i.e. dot character
-		*  $  matches end of line
-		*/
 		if(str==null)return  false;
 		String rgx = "^[A-Z].*[\\.]$";
 		Pattern p = Pattern.compile(rgx);
